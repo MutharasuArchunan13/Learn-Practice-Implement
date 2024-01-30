3e164fee1f90f0b4543c43a75e5287fb86075b03
MutharasuArchunan13 <mutharasu39@gmail.com>
2024-01-30 08:20:14 +0530

    learned top k frquent element

--- Data-Structure-Algorithms/top_k_frquent_element.py
+++ Data-Structure-Algorithms/top_k_frquent_element.py
@@ -10,8 +10,21 @@ class Solution:
                 frequent_dict[num] +=1
             else:
                 frequent_dict[num] = 1
-        sorted_dict = sorted(frequent_dict.items(), key=lambda x: x[1], reverse=True)
+        sorted_dict = dict(sorted(frequent_dict.items(), key=lambda x: x[1], reverse=True))
+        print(sorted_dict)
+        #must return the top k elements only
+        count = 0
+        for key,value in sorted_dict.items():
+            result.append(key)
+            count +=1
+            if count == k:
+                break
+        
+        return result
+
+
 
         
 class_obj = Solution()
-class_obj.topKFrequent([1,1,1,1,2,2,3,3,3],2)
+result = class_obj.topKFrequent([1,1,1,1,2,2,3,3,3,3,3,3,3],2)
+print(f"The top k frequent elements: {result}")
