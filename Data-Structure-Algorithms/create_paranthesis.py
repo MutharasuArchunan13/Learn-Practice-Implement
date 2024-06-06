ccf5ef9129b3d19e10dfa51669becc9437f84b21
mutharasu A <mutharasu39@gmail.com>
2024-06-06 07:57:37 +0530

    recursion got idean need to improve

--- Data-Structure-Algorithms/create_paranthesis.py
+++ Data-Structure-Algorithms/create_paranthesis.py
@@ -1,16 +1,36 @@
 from typing import List
-def generateParenthesis(self, n: int) -> List[str]:
-	def dfs(left, right, s):
-		if len(s) == n * 2:
-			res.append(s)
-			return 
+# def generateParenthesis(self, n: int) -> List[str]:
+# 	def dfs(left, right, s):
+# 		if len(s) == n * 2:
+# 			res.append(s)
+# 			return 
 
-		if left < n:
-			dfs(left + 1, right, s + '(')
+# 		if left < n:
+# 			dfs(left + 1, right, s + '(')
 
-		if right < left:
-			dfs(left, right + 1, s + ')')
+# 		if right < left:
+# 			dfs(left, right + 1, s + ')')
 
-	res = []
-	dfs(0, 0, '')
-	return res
+# 	res = []
+# 	dfs(0, 0, '')
+# 	return res
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack=[]
+        res=[]
+        def paran(left,right):
+            if left==right==n:
+                res.append("".join(stack))
+                return
+            if left < n:
+                stack.append("(")
+                paran(left+1,right)
+                stack.pop()
+            if right < left:
+                stack.append(")")
+                paran(left,right+1)
+                stack.pop()
+        paran(0,0)
+        return res
+        
+Solution().generateParenthesis(2)
